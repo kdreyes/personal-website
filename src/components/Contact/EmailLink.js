6e59b9881b56eec4712ffe671aa2477feae2cd0b
kdreyes <katrina.mdelosreyes@gmail.com>
2021-01-01 20:34:35 -0500

    personalized info

--- src/components/Contact/EmailLink.js
+++ src/components/Contact/EmailLink.js
@@ -77,7 +77,7 @@ const EmailLink = () => {
       onMouseEnter={() => setIsActive(false)}
       onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
     >
-      <a href={validateText(message) ? `mailto:katrina.mdelosreyes@gmail.com` : ''}>
+      <a href={validateText(message) ? 'mailto:katrina.mdelosreyes@gmail.com' : ''}>
         <span>{message}</span>
         <span>@katrinadelosreyes.com</span>
       </a>
